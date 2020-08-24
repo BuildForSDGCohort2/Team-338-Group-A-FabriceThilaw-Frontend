ecf286e64fc5fa163f597f64cf9f7afdf9c1a1ad
Fabrice Thilaw <fabricethilaw@users.noreply.github.com>
2020-08-25 00:56:17 +0100

    feat: add login page

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from "@angular/common";
 import { SharedModule } from '../shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
     imports: [
@@ -13,7 +14,7 @@ import { AuthenticationRoutingModule } from './authentication-routing.module';
     ],
     declarations: [
         
-    ]
+    LoginComponent]
 })
 
 export class AuthenticationModule {}
